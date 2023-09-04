7c59beacd4630ba1762e1bba62783e7ca6793930
Eliezer Belloso <Eliezer Belloso@PC-ELIEZER>
2023-09-04 08:08:14 -0600

    Agregar archivos de proyecto.

---
+++ DATA_5834312/Alumnos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATA_5834312
+{
+    public class Alumnos: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string nombre = string.Empty;
+
+        public string name {
+            get => nombre;
+             set
+            {
+                if (nombre == value)
+                    return;
+                nombre = value;
+                onPropertyChanged(nameof(name));
+                onPropertyChanged(nameof(MostrarNombre));
+            }
+        }
+
+        public string MostrarNombre => $"Nombre ingresado: {name}";
+
+        void onPropertyChanged(string nombre)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+    }
+}
---
+++ DATA_5834312/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DATA_5834312;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
